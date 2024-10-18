93ad5bc406ba408776e89421099133ecaa94a480
unknown <BT_3W2_27@BKTECH.LOCAL>
2024-10-18 15:18:05 -0400

    finished

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 // SIHENG'S STUDENT COUNTER
 let counter = document.getElementById("count-el");
 counter.innerText = 0;
-
+let saveP = document.getElementById("save");
+let audio = new Audio("cat-meow.mp3");
+let x;
+stack = 0;
+stop = false
 function increment() {
     counter.innerText = Number(counter.innerText) + 1;
-    console.log(counter.innerText)
+    console.log(counter.innerText);
+}
+function save() {
+    saveP.innerText = saveP.innerText + counter.innerText + " - ";
+    console.log(saveP.innerText + counter.innerText + " - ");
+}
+function meow() {
+    audio.play();
+}
+function incrementByYourSelf() {
+    x = setInterval(increment, 1000)
+}
+function stopPlease() {
+    console.log("STOP")
+    clearInterval(x);
 }
